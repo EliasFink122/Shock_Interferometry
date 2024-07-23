ba74dbadac806e5e0d6192ca5fa750b7668dec6b
Elias Fink <elias.fink@gmx.de>
2024-07-23 09:48:11 +0100

    Doppler conversion

--- SI_1D.py
+++ SI_1D.py
@@ -6,11 +6,15 @@ Created on Tue Jul 23 2024
 Simulation of 1D interferogram trace.
 
 Methods:
-
+    read_in:
+        read in data from csv file
+    convert_doppler:
+        convert speeds to Doppler shifted wavelengths
 """
 import numpy as np
+from scipy.constants import speed_of_light as c
 
-def read_in(path: str) -> np.array:
+def read_in(path = "data.csv") -> np.array:
     '''
     Read in shock speed data.
 
@@ -20,3 +24,21 @@ def read_in(path: str) -> np.array:
     Returns:
         data in numpy array
     '''
+    data = np.loadtxt(path, delimiter=',', skiprows=1)
+    return data
+
+def convert_doppler(wavelength: float, speeds: np.ndarray) -> np.ndarray:
+    '''
+    Convert speeds to Doppler shifted wavelength.
+
+    Args:
+        wavelength: unshifted laser wavelength
+        speeds: speed array from data
+
+    Returns:
+        shifted wavelengths as array
+    '''
+    betas = speeds/c
+    new_wavelengths = wavelength * np.sqrt((1+betas)/(1-betas))
+
+    return new_wavelengths
