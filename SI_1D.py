e9b63b2e151ca304a9479829c76cb19ea8852ad9
Elias Fink <elias.fink@gmx.de>
2024-07-23 10:12:43 +0100

    Interferogram

--- SI_1D.py
+++ SI_1D.py
@@ -32,7 +32,7 @@ def convert_doppler(wavelength: float, speeds: np.ndarray) -> np.ndarray:
     Convert speeds to Doppler shifted wavelength.
 
     Args:
-        wavelength: unshifted laser wavelength
+        wavelength: unshifted laser wavelength in m
         speeds: speed array from data
 
     Returns:
@@ -42,3 +42,24 @@ def convert_doppler(wavelength: float, speeds: np.ndarray) -> np.ndarray:
     new_wavelengths = wavelength * np.sqrt((1+betas)/(1-betas))
 
     return new_wavelengths
+
+def interferogram(wavelength: float, opt_depth = 1) -> np.ndarray:
+    '''
+    Generate Mach Zehnder interferogram.
+
+    Args:
+        opt_depth: optical depth difference of second arm
+        wavelength: wavelength of light
+
+    Returns:
+        positions of fringes
+    '''
+    period = wavelength/(2*opt_depth*np.sin(np.pi/10))
+    x = np.linspace(-10*period, 10*period, 100)
+    fringes = np.sin(x*2*np.pi/period)
+
+    return fringes
+
+if __name__ == "__main__":
+    speed_arr = read_in()
+    wavelengths = convert_doppler(5.5e-7, speed_arr)
